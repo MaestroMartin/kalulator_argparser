2f99a1b88674a295253d2cf16e2f756d8b47879f
Martin Pelíšek <161080106+MaestroMartin@users.noreply.github.com>
2024-11-26 20:35:58 +0100

    next add

--- Kalkulator.py
+++ Kalkulator.py
@@ -2,33 +2,30 @@ import math
 import argparse
 
 
-choice1 = float or int(input("Enter your number: "))
+choice1 = float(input("Enter your number: "))
 
 operations = input("enter your operations => /, *, -, + = :")
-
-choice2 = float or int(input("Enter your second number: "))
+operators = ("/","*","-" )
+choice2 = float(input("Enter your second number: "))
 
 def result():
-    if choice1 and choice2 == float or int:
-        next 
+    if "+" in operations:
+        print(choice1 + choice2)
+        next
     else:
-        exit    	
-        if "+" in operations:
-            print(choice1 + choice2)
-            next
+        if "-" in operations:
+            print(choice1 - choice2)
+            next 
         else:
-            if "-" in operations:
-                print(choice1 - choice2)
-                next 
+            if "*" in operations:
+                print(choice1 * choice2)
+                next
             else:
-                if "*" in operations:
-                    print(choice1 * choice2)
-                    next
+                if "/" in operations:
+                    choice1 and choice2 != 0
+                    breakpoint
                 else:
-                    if "/" in operations:
-                        choice1 and choice2 != 0
-                        breakpoint
-                    else:
-                        print(choice1 / choice2)
-            
+                    print(choice1 / choice2)
+
+result()
 
