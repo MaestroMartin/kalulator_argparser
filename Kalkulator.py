2bf19accca3fd7530d92c94bfe0727ad0a2c4a52
Martin Pelíšek <161080106+MaestroMartin@users.noreply.github.com>
2024-11-26 20:20:19 +0100

    Creat kalkulator

---
+++ Kalkulator.py
@@ -0,0 +1,34 @@
+import math
+import argparse
+
+
+choice1 = float or int(input("Enter your number: "))
+
+operations = input("enter your operations => /, *, -, + = :")
+
+choice2 = float or int(input("Enter your second number: "))
+
+def result():
+    if choice1 and choice2 == float or int:
+        next 
+    else:
+        exit    	
+        if "+" in operations:
+            print(choice1 + choice2)
+            next
+        else:
+            if "-" in operations:
+                print(choice1 - choice2)
+                next 
+            else:
+                if "*" in operations:
+                    print(choice1 * choice2)
+                    next
+                else:
+                    if "/" in operations:
+                        choice1 and choice2 != 0
+                        breakpoint
+                    else:
+                        print(choice1 / choice2)
+            
+
