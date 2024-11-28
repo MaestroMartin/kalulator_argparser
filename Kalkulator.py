745f18b0b4ff28adf2ae1807a04fab9baf4c5d30
Martin Pelíšek <161080106+MaestroMartin@users.noreply.github.com>
2024-11-28 18:18:59 +0100

    better Kalkulator

--- Kalkulator.py
+++ Kalkulator.py
@@ -2,31 +2,49 @@ import math
 import argparse
 
 
-choice1 = float(input("Enter your number: "))
+def addition(number1,number2):
+    return number1 + number2
 
-operations = input("enter your operations => /, *, -, + = :")
+def substraction(number1,number2):
+    return number1 - number2 
 
-choice2 = float(input("Enter your second number: "))
+def multiplication(number1,number2):
+    return number1 * number2
 
-def result():
-    if "+" in operations:
-        print(choice1 + choice2)
-        next
+def division(number1,number2):
+    if number2 != 0:
+        return number1 /number2
     else:
-        if "-" in operations:
-            print(choice1 - choice2)
-            next 
-        else:
-            if "*" in operations:
-                print(choice1 * choice2)
-                next
-            else:
-                if "/" in operations:
-                    choice1 and choice2 != 0
-                    breakpoint
-                    print("you dont division by 0")
-                else:
-                    print(choice1 / choice2)
+        return ValueError
 
-result()
+def kalkulator():
+    print("Velcome in Kalkulator, please choose number: ")
+    print("1 = addition ")
+    print("2 = substraction")
+    print("3 = multiplication")
+    print("4 = division")
+
+    try: 
+        number1 = float(input("enter your first number: "))
+        choose = int(input())
+        number2 = float(input("enter your second number: "))
+
+        if choose == 1:
+            print( f"result is {addition(number1,number2)}")
+
+        if choose == 2:
+            print( f"result is {substraction(number1,number2)}")
+
+        if choose == 3:
+            print( f"result is {multiplication(number1,number2)}")  
+
+        if choose == 4:
+             print( f"result is {division(number1,number2)}")
+
+    except  ValueError:
+        print("EROR: entered values not right numbers")
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
 
